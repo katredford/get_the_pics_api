3e6118e466ccb81068ce69a92209cccb6fb65856
katredford <katharine.redford@gmail.com>
2023-05-30 15:35:38 -0500

    can now add pics

--- routes/api/picRoutes.js
+++ routes/api/picRoutes.js
@@ -19,17 +19,33 @@ const connection = mysql.createConnection(
 // HANDLER is the function executed when the route is matched.
 // var router = express.Router()
 
-router.route('/')
-  .all(function (req, res, next) {
-    // runs for all HTTP verbs first
-    // think of it as route specific middleware!
-    next()
-  })
+// router.route('/')
+//   // .all(function (req, res, next) {
+//   //   // runs for all HTTP verbs first
+//   //   // think of it as route specific middleware!
+//   //   next()
+//   // })
   
-  .get((req, res) => {
+  router.get( "/", (req, res) => {
   const sql = `SELECT * FROM pics`;
   
 connection.query(sql, (err, result) => {
+
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: result
+    });
+  });
+  });
+
+  router.post( "/",({body}, res) => {
+  const sql = `INSERT INTO pics (title, url, description) VALUES (?, ?, ?)`;
+  const addPics = [body.title, body.url, body.description]
+connection.query(sql, addPics, (err, result) => {
 
     if (err) {
       res.status(500).json({ error: err.message });
